da636f829ab5f8770cb023f92c8ee3c61b82d622
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:16:57 +0200

    message ends with ...

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         String message = "Hello World" + "!!";
-        System.out.println(message);
+        System.out.println(message.endsWith("!!"));
     }
 }
