e90aeb16931834e8723ed2d85f188356b993c55a
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:12:04 +0200

    reference types

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -1,11 +1,11 @@
 package com.nadine;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        byte x = 1;
-        byte y = x;
-        x = 2;
-        System.out.println(y);
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
     }
 }
