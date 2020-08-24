1018f844e19b462a0460b2f44bb7ef1a9aab849a
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 20:54:27 +0200

    declaring and initializing variables

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -3,8 +3,8 @@ package com.nadine;
 public class Main {
 
     public static void main(String[] args) {
-        int age = 30;
-        age = 35;
-	    System.out.println(age);
+        int myAge = 30;
+        int herAge = myAge;
+	    System.out.println(herAge);
     }
 }
