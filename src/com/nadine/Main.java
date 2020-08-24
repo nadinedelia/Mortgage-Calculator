5f8f807ff4089cdf67046d68c80908cf15279d7b
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:17:54 +0200

    checking length of string

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         String message = "Hello World" + "!!";
-        System.out.println(message.endsWith("!!"));
+        System.out.println(message.length());
     }
 }
