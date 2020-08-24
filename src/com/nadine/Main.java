86cd14761b87fc7f47e007f71728a0ca6239322d
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:39:10 +0200

    displaying length of array

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -6,10 +6,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[2] = 2;
-
-        System.out.println(Arrays.toString(numbers));
+        int[] numbers = { 2, 3, 5, 1, 4 };
+        System.out.println(numbers.length);
     }
 }
