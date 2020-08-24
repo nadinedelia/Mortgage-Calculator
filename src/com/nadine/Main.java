1d6431b9a6fc86d35f09354cff438475aa6be1dc
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:00:02 +0200

    F for float

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         byte age = 30;
         long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
 	    System.out.println(viewsCount);
     }
 }
