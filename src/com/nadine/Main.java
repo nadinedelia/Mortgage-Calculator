d7c599fea69e4ab06c88a28aeb3255987c54092f
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:15:11 +0200

    shorthand for initializing strings

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -5,9 +5,7 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        String message = "Hello World";
+        System.out.println();
     }
 }
