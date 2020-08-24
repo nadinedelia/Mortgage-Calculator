82ed6d24efcf8128b2736be3062651024e33f8ab
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:25:52 +0200

    double backslash to display single backslash

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Looking for \"Pizza\"";
+        String message = "c:\\windows\\...";
         System.out.println(message);
     }
 }
