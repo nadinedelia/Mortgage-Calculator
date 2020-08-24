3f69cc56a6f3347981dca6d8a9084744d1a4943e
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 20:52:10 +0200

    assigning variables

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -3,6 +3,7 @@ package com.nadine;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+        int age = 30;
+	    System.out.println(age);
     }
 }
