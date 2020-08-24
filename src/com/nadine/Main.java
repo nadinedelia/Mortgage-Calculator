3c5e9be336e4c056cbc9bbb22cda6ff538f658c6
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:07:29 +0200

    date object

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -1,13 +1,12 @@
 package com.nadine;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-	    System.out.println(isEligible);
+        Date now = new Date();
+        System.out.println(now);
     }
 }
