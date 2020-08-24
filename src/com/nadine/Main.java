ca09af508f7c39f7c2e36489cdcd80e5677f026c
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:15:53 +0200

    concatinate strings

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Hello World";
-        System.out.println();
+        String message = "Hello World" + "!!";
+        System.out.println(message);
     }
 }
