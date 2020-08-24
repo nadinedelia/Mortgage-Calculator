cc8a3c766ae728d87d5784654dbed9aa89a3e20b
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:41:42 +0200

    sorting array

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         int[] numbers = { 2, 3, 5, 1, 4 };
-        System.out.println(numbers.length);
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
     }
 }
