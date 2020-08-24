14bd16c45188201ac0aaab8e6b16863ac52400b2
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:13:25 +0200

    reference types are copied by their reference

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Point point1 = new Point(1,1);
         Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
