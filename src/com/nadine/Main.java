1f3b496f008bca03fff2e2d94a1700b55b9c85ae
nadinedelia <nadine.delia@yahoo.com>
2020-08-24 21:18:54 +0200

    index of

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         String message = "Hello World" + "!!";
-        System.out.println(message.length());
+        System.out.println(message.indexOf("e"));
     }
 }
