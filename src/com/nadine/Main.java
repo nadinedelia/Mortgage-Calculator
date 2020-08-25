4aa5a6cc1942586e2469e1be2774331f29e58463
nadinedelia <nadine.delia@yahoo.com>
2020-08-25 16:21:59 +0200

    incrementing

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -6,7 +6,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int result = 10 + 3;
-        System.out.println(result);
+        int x = 1;
+        int y = x++;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
