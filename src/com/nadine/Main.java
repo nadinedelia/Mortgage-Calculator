5f8bc60e75eef61e7d46914fee9176a8fd7d6a63
nadinedelia <nadine.delia@yahoo.com>
2020-08-25 13:27:30 +0200

    printing multidimensional arrays

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -6,9 +6,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = { 2, 3, 5, 1, 4 };
-        Arrays.sort(numbers);
-
-        System.out.println(Arrays.toString(numbers));
+        int[][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
