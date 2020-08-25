a764783be6c6a5dada87ffebd8c381b4324e9e45
nadinedelia <nadine.delia@yahoo.com>
2020-08-25 16:46:14 +0200

    casting

--- src/com/nadine/Main.java
+++ src/com/nadine/Main.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 1;
-        x += 2;
-        System.out.println(x);
+        double x = 1.1;
+        double y = x + 2;
+        System.out.println(y);
     }
 }
